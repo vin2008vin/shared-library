e55b6a53ba37cf709c1200df73afe2390662f2bd
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-04 00:59:38 +0530

    Update jenkins-sharedlibrary.groovy

--- vars/jenkins-sharedlibrary.groovy
+++ vars/jenkins-sharedlibrary.groovy
@@ -14,38 +14,3 @@ def call(Map args=[:]) {
     }
     return this
 }
-
-def compile(Map args) {
-    node {
-        stage('Logging into AWS ECR') {
-            sh "aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 647834768285.dkr.ecr.ap-south-1.amazonaws.com"
-        }
-    }
-    return this
-}
-
-def buildDockerImage(Map args) {
-    node {
-        def dockerEcr = new DockerEcr(this)
-        stage('Build Docker Image') {
-            docker.build sample:0.1.0
-        }
-    }
-    return this
-}
-
-def publishDockerImage(Map args) {
-    node {
-        stage('Publish Docker Image') {
-            sh "docker tag sample:0.1.0 647834768285.dkr.ecr.ap-south-1.amazonaws.com/sample:0.1.0"
-            sh "docker push 647834768285.dkr.ecr.ap-south-1.amazonaws.com/sample:0.1.0"
-        }
-    }
-    return this
-}
-
-def additionalPostBuildSteps(Closure body= { }) {
-    node {
-        body()
-    }
-}
