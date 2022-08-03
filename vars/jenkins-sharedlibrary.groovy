c66768f07386987424624b425a0f5bac7072f709
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-04 01:15:21 +0530

    Update jenkins-sharedlibrary.groovy

--- vars/jenkins-sharedlibrary.groovy
+++ vars/jenkins-sharedlibrary.groovy
@@ -4,7 +4,7 @@ import jenkins.model.*
 import groovy.json.JsonSlurper
 import groovy.json.JsonBuilder
 import com.buildah.federated.buildcontainers.stages.*
-import groovy.*
+package groovy.lang
 
 
 def call(Map args=[:]) {
