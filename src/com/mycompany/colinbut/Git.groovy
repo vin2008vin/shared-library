9ed29357082c3a606be67221c070a16f0ae13066
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-04 18:42:04 +0530

    Update Git.groovy

--- src/com/mycompany/colinbut/Git.groovy
+++ src/com/mycompany/colinbut/Git.groovy
@@ -9,7 +9,7 @@ class Git implements Serializable {
     }
 
     def checkout(String repo) {
-        this.script.git credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/vin2008vin/shared-library.git"
+        this.script.git credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/vin2008vin/${repo}.git"
     }
 
     String commitHash() {
