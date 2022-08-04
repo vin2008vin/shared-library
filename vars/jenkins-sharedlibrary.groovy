6d3c6ed5489527378b4e027b5e50efe7bffddc96
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-04 15:37:02 +0530

    Update jenkins-sharedlibrary.groovy

--- vars/jenkins-sharedlibrary.groovy
+++ vars/jenkins-sharedlibrary.groovy
@@ -1,13 +1,6 @@
 import com.mycompany.colinbut.*
 import com.mycompany.colinbut.Git*
 import jenkins.model.*
-import groovy.json.JsonSlurper
-import groovy.json.JsonBuilder
-import com.buildah.federated.buildcontainers.stages.*
-import groovy.lang.Binding
-import groovy.lang.GroovyShell
-package groovy.lang
-
 
 def call(body) {
     node {
